b4e23f4790f866ca31db1b78effff0786986b35d
Dinesh Budhathoki <physmarika@gmail.com>
2025-04-30 18:50:05 +0800

    Navbar component is finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div>Ishikiya is coming soon....</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="bg-white/80 shadow-md fixed top-0 left-0 w-full z-40 ease-in duration-300 backdrop-blur-md">
+      <div className="py-3 px-10 sm:px-4 md:px-6 lg:px-6 container mx-auto">
+        <div className="flex items-center justify-between ">
+          <img src="./logo.png" alt="" className="h-14 cursor-pointer" />
+          {/* All links  */}
+          <div className="lg:flex hidden gap-8 items-center">
+            <a
+              href=""
+              className="text-[#191919] text-xl font-medium hover:text-red-500"
+            >
+              Today Special
+            </a>
+            <a
+              href=""
+              className="text-[#191919] text-xl font-medium hover:text-red-500"
+            >
+              About Us
+            </a>
+            <a
+              href=""
+              className="text-[#191919] text-xl font-medium hover:text-red-500"
+            >
+              Our Menu
+            </a>
+            <a
+              href=""
+              className="text-[#191919] text-xl font-medium hover:text-red-500"
+            >
+              Add Menu
+            </a>
+            <a
+              href=""
+              className="text-[#191919] text-xl font-medium hover:text-red-500"
+            >
+              Popular Food
+            </a>
+            <button className="bg-[#F54748] active:scale-90 transition duration-100 transform hover:shadow-xl shadow-md rounded-full px-8 py-2 text-xl font-medium text-white cursor-pointer">
+              Login
+            </button>
+          </div>
+          {/* Hamburgur  */}
+          <div className="block lg:hidden z-40" onClick={handleNav}>
+            {nav ? (
+              <RxCross2 size={25} className="text-red-500 cursor-pointer" />
+            ) : (
+              <GiHamburgerMenu
+                size={25}
+                className="text-[#191919] cursor-pointer"
+              />
+            )}
+          </div>
+          {/* Menu items  */}
+          <div
+            className={`lg:hidden absolute w-1/2 sm:w-2/5 h-[70vh] px-4 py-2 font-medium ease-in shadow-sm backdrop-blur-md bg-white/80 top-0 duration-500 ${
+              nav ? "right-0" : "right-[-100%]"
+            } pt-24 rounded-xl`}
+          >
+            <div className="flex flex-col gap-8">
+              <a
+                href=""
+                className="text-[#191919] text-xl font-medium hover:text-red-500"
+              >
+                Today Special
+              </a>
+              <a
+                href=""
+                className="text-[#191919] text-xl font-medium hover:text-red-500"
+              >
+                About Us
+              </a>
+              <a
+                href=""
+                className="text-[#191919] text-xl font-medium hover:text-red-500"
+              >
+                Our Menu
+              </a>
+              <a
+                href=""
+                className="text-[#191919] text-xl font-medium hover:text-red-500"
+              >
+                Add Menu
+              </a>
+              <a
+                href=""
+                className="text-[#191919] text-xl font-medium hover:text-red-500"
+              >
+                Popular Food
+              </a>
+              <button className="bg-[#F54748] active:scale-90 transition duration-100 transform hover:shadow-xl shadow-md rounded-full px-8 py-2 text-xl font-medium text-white cursor-pointer">
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
