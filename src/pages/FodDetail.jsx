d584d8e1f3b6a034908175ffedb6bb9f4b4b4078
Dinesh Budhathoki <physmarika@gmail.com>
2025-05-07 13:58:36 +0800

    Food details page is edited

--- src/pages/FodDetail.jsx
+++ src/pages/FodDetail.jsx
@@ -1,10 +1,13 @@
 import PageNavigation from "@/components/PageNavigation";
 import { getFood } from "@/store/admin/foodSlice";
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { AiOutlineMinus } from "react-icons/ai";
+import { HiOutlinePlus } from "react-icons/hi";
 
 const FodDetail = () => {
+  const { user } = useSelector((state) => state.auth);
   const [foodDetails, setFoodDetails] = useState(null);
   const dispatch = useDispatch();
   const { id } = useParams();
@@ -36,6 +39,7 @@ const FodDetail = () => {
     <div className="pt-[16vh] px-10 sm:px-4 md:px-6">
       <div className="container w-full py-8">
         <PageNavigation title={foodDetails?.title} />
+        {/* This is for rendering food details  */}
         <div className="grid grid-cols-1 md:grid-cols-2 pb-14 gap-8 ">
           <div className="shadow-md p-4 rounded-md">
             <img
@@ -44,17 +48,44 @@ const FodDetail = () => {
               className="w-full h-[25rem] object-cover rounded"
             />
           </div>
-          <div className="rounded p-8 text-black mb-5">
-            <h2 className="text-2xl font-bold text-center text-blue-600 mb-4">
+          <div className="rounded bg-red-200/[.3] p-8 text-black mb-5 flex-1">
+            <h2 className="text-2xl font-bold text-blue-600 mb-4">
               {foodDetails?.title}
             </h2>
-            <p className="text-gray-700 mt-4">{foodDetails?.description}</p>
-            <div className="mt-4 text-xl font-semibold">
+            <h2 className="text-xl font-bold text-blue-600 mb-4">
               Price: ${foodDetails?.price}
+            </h2>
+            <p className="text-gray-700 mt-4">{foodDetails?.description}</p>
+            <div className="flex items-center justify-between mb-6 mt-3">
+              <div className="text-2xl font-bold text-red-400">Quantity</div>
+              <span className="flex items-center space-x-4">
+                <div className="bg-red-500 relative p-4 rounded-full text-white">
+                  <AiOutlineMinus
+                    className=" cursor-pointer font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                    size={20}
+                  />
+                </div>
+                <span className="text-red-500 px-6 py-2 bg-slate-50 text-lg font-medium">
+                  1
+                </span>
+                <div className="bg-red-500 relative p-4 rounded-full text-white">
+                  <HiOutlinePlus
+                    className=" cursor-pointer font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                    size={20}
+                  />
+                </div>
+              </span>
             </div>
+            {user?.role === "admin" ? (
+              <div className="flex gap-4">
+                <button className="mt-4 btn btn-primary">Edit</button>
+                <button className="mt-4 btn btn-primary">Delete</button>
+              </div>
+            ) : (
+              <button className="mt-4 btn btn-primary">Order Now</button>
+            )}
           </div>
         </div>
-        <button className="mt-4 btn btn-primary">Order Now</button>
       </div>
     </div>
   );
